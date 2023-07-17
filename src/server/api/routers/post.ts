6bbea8e261ca49786935fd04fb773f51dc211f33
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-07-17 16:41:04 +0300

    post findbyId

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,4 +5,7 @@ export const postRouter = createTRPCRouter({
     getAll: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.post.findMany();
     }),
+    getById: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+        return ctx.prisma.post.findUnique({ where: { id: input } })
+    })
 });
