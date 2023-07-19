1aa221aa970bba548e16b5265183173f6ae1dda8
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-07-19 15:51:06 +0300

    head

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const AboutPage: NextPage = () => {
-  return <div>works?</div>;
+  return (
+    <section>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Arya Açıkgöz" key={'author'}></meta>
+        <title key={'title'}>About - Arya Açıkgöz - Arya Trying to Code</title>
+      </Head>
+      <div>asd</div>
+    </section>
+  );
 };
 
 export default AboutPage;
