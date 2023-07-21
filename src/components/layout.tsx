034331cd13077f4b4cd44bfad9d56ff43ac44a38
Arya A <aryaacikgoz@outlook.com>
2023-07-21 16:21:05 +0300

    dark mode improvements

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from './navbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
+    <ThemeProvider attribute="class" enableSystem={false} enableColorScheme>
       <Navbar />
       {children}
     </ThemeProvider>
--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import type { FC } from 'react';
+import { BsMoon, BsFillSunFill } from 'react-icons/bs';
 
 const ThemeToggler: FC = () => {
   const [mounted, setMounted] = useState(false);
@@ -18,14 +19,22 @@ const ThemeToggler: FC = () => {
   };
 
   return (
-    <div className="toggle-wrapper">
+    <div>
       <input
         type="checkbox"
-        id="theme-toggle"
+        id="checkbox"
+        className="group absolute cursor-pointer p-3 opacity-0 checked:translate-x-6"
         checked={theme === 'dark' && mounted ? false : true}
         onChange={handleTheme}
       />
-      <label htmlFor="theme-toggle"></label>
+      <label htmlFor="checkbox" className="h-full w-full cursor-pointer">
+        {theme === 'light' && mounted && (
+          <BsMoon className="text-xl text-[#0D0D0D]" />
+        )}
+        {theme === 'dark' && mounted && (
+          <BsFillSunFill className="text-xl text-yellow-400" />
+        )}
+      </label>
     </div>
   );
 };
