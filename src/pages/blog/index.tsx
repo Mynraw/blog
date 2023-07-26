304959af34bf456f2511ff1cf123f7148cc6a422
Arya A <aryaacikgoz@outlook.com>
2023-07-26 17:08:46 +0300

    page name and auth middleware has changed

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const BlogPage: NextPage = () => {
+  return (
+    <section className="min-h-screen">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Arya Açıkgöz" key={'author'}></meta>
+        <title key={'title'}>About - Arya Açıkgöz - Arya Trying to Code</title>
+      </Head>
+      <Image
+        className="mx-auto mt-16"
+        src={'/wip.png'}
+        alt="lfg"
+        width={420}
+        height={420}
+      />
+    </section>
+  );
+};
+
+export default BlogPage;
--- src/pages/post/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next';
-
-const PostPage: NextPage = () => {
-  return <div>works</div>;
-};
-
-export default PostPage;
