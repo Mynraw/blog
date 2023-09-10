50a7ae46abdbef89e08ed26511e571f3d0a25a74
Arya A <aryaacikgoz@outlook.com>
2023-09-10 12:22:50 +0300

    mobile navbar refactorings

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -7,23 +7,23 @@ const MobileNavbar: FC = () => {
   return (
     <div className="sticky bottom-0 w-full overflow-hidden border-t border-primary border-opacity-20 backdrop-blur-lg supports-[backdrop-blur]:bg-white/95 dark:border-secondary dark:border-opacity-20 sm:hidden">
       <div className="mx-auto">
-        <nav className="flex items-center justify-evenly">
-          <Link className="px-2 py-3" href={'/'}>
-            <LuHome className="text-2xl" />
+        <nav className="flex items-center justify-evenly py-1">
+          <Link className="p-2" href={'/'}>
+            <LuHome className="text-xl" />
           </Link>
 
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
 
-          <Link className="px-2 py-3" href={'/blog'}>
-            <LuBookMarked className="text-2xl" />
+          <Link className="p-2" href={'/blog'}>
+            <LuBookMarked className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
-          <Link className="px-2 py-3" href={'/about'}>
-            <LuCode2 className="text-2xl" />
+          <Link className="p-2" href={'/about'}>
+            <LuCode2 className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
-          <Link className="px-2 py-3" href={'/resume'}>
-            <LuFileBadge className="text-2xl" />
+          <Link className="p-2" href={'/resume'}>
+            <LuFileBadge className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
 
--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -26,20 +26,20 @@ const ThemeToggler: FC<ThemeTogglerProps> = (props) => {
     <button
       type="button"
       id="checkbox"
-      className={`cursor-pointer ${mobileResolution && 'px-2 py-3'}`}
+      className={`cursor-pointer ${mobileResolution && 'p-2'}`}
       onClick={handleTheme}
     >
       {theme === 'light' && mounted && (
         <BsMoonStarsFill
           className={`cursor-pointer text-[#0D0D0D] ${
-            mobileResolution ? 'text-2xl' : 'text-lg'
+            mobileResolution ? 'text-xl' : 'text-lg'
           }`}
         />
       )}
       {theme === 'dark' && mounted && (
         <LuSun
           className={`cursor-pointer text-[#E59A00] ${
-            mobileResolution ? 'text-2xl' : 'text-lg'
+            mobileResolution ? 'text-xl' : 'text-lg'
           }`}
         />
       )}
