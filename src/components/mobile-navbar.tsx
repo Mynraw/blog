bc6a2c8f690e388f1da9a462c64b3659109a4302
Arya A <aryaacikgoz@outlook.com>
2023-09-10 16:47:04 +0300

    routing fix

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -8,19 +8,19 @@ const MobileNavbar: FC = () => {
     <div className="sticky bottom-0 w-full overflow-hidden border-t border-primary border-opacity-20 backdrop-blur-lg supports-[backdrop-blur]:bg-white/95 dark:border-secondary dark:border-opacity-20 sm:hidden">
       <div className="mx-auto">
         <nav className="flex items-center justify-evenly py-2">
-          <Link className="p-2" href={'/'}>
+          <Link className="p-2" href="/">
             <LuHome className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
-          <Link className="p-2" href={'/blog'}>
+          <Link className="p-2" href="/blog">
             <LuBookMarked className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
-          <Link className="p-2" href={'/about'}>
+          <Link className="p-2" href="/about">
             <LuCode2 className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
-          <Link className="p-2" href={'/resume'}>
+          <Link className="p-2" href="/resume">
             <LuFileBadge className="text-xl" />
           </Link>
           <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
