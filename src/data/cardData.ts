81c3cc638cecf1436437c220b5105ae0dac08bf7
Arya A <aryaacikgoz@outlook.com>
2023-09-10 17:48:09 +0300

    data revised

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -2,10 +2,10 @@ import type { CardDataProps } from "~/types/cardData"
 
 export const cardData: CardDataProps = {
     name: 'Arya Açıkgöz',
-    shorter: 'Developer | Learner | Memer',
+    shorter: 'Developer | Learner',
     location: 'Ankara, Turkey',
     title: 'FrontEnd Developer',
     company: 'TK Technology',
     email: 'aryaacikgoz@outlook.com',
-    socials: { twitter: 'aryaacikgoz', github: 'mynraw', linkedIn: 'aryaacikgoz', spotify: 'snuksnuk' }
+    socials: { twitter: 'typeofvirtual', github: 'mynraw', linkedIn: 'aryaacikgoz', spotify: 'snuksnuk' }
 }
