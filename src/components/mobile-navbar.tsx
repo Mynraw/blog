9956d563888297aebee14c2516770ac23b7b4a5e
Arya A <aryaacikgoz@outlook.com>
2023-09-20 22:37:48 +0300

    mobile nav

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -7,23 +7,20 @@ const MobileNavbar: FC = () => {
   return (
     <div className="sticky bottom-0 w-full overflow-hidden border-t border-primary border-opacity-20 backdrop-blur-lg supports-[backdrop-blur]:bg-white/95 dark:border-secondary dark:border-opacity-20 sm:hidden">
       <div className="mx-auto">
-        <nav className="flex items-center justify-evenly py-2">
+        <nav className="flex items-center justify-evenly pb-3.5 pt-2">
           <Link className="p-2" href="/">
             <LuHome className="text-xl" />
           </Link>
-          <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
+          {/* <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" /> supposed to be a divider */}
           <Link className="p-2" href="/blog">
             <LuBookMarked className="text-xl" />
           </Link>
-          <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
           <Link className="p-2" href="/snippets">
             <LuCode2 className="text-xl" />
           </Link>
-          <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
           <Link className="p-2" href="/resume">
             <LuFileBadge className="text-xl" />
           </Link>
-          <div className="h-7 border-r border-primary opacity-30 dark:border-secondary" />
           <ThemeToggler mobileResolution />
         </nav>
       </div>
