c9ef4c1b2ff59f1ca4924861abc54fdc8b20af00
Arya A <aryaacikgoz@outlook.com>
2024-01-06 12:55:40 +0300

    testing href

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import Image from 'next/image';
 import NavigationItems from './navigation-items';
 
@@ -8,7 +8,7 @@ const Navbar: FC = () => {
     <div className="sticky top-0 z-10 w-full overflow-hidden py-2 font-semibold backdrop-blur-lg supports-[backdrop-blur]:bg-white/95 xs:hidden">
       <div className="mx-auto flex max-w-5xl items-center justify-between px-3 md:px-6 3xl:max-w-[80%]">
         <div>
-          <Link href="/" shallow>
+          <NextLink href="/" shallow>
             <Image
               src="https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18yU2hGVVdzMnJBM2J0ZTZGeE8yN2VsY1R0UFUuZ2lmIn0"
               alt="Mynraw"
@@ -17,7 +17,7 @@ const Navbar: FC = () => {
               priority
               className="rounded-full"
             />
-          </Link>
+          </NextLink>
         </div>
         <NavigationItems />
       </div>
