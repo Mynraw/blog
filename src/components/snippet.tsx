5873d88ed1b61287fde8d8cf7383fa762c561260
Arya A <aryaacikgoz@outlook.com>
2024-01-14 20:18:22 +0300

    overflow fix on snippet items

--- src/components/snippet.tsx
+++ src/components/snippet.tsx
@@ -9,11 +9,11 @@ const Snippet: FC<SnippetProps> = (props) => {
   return (
     <Link href={`/snippets/${slug}`} key={slug}>
       <div className="flex items-center gap-x-6 rounded-xl border border-transparent p-4 text-primary shadow-intense shadow-primary transition-all hover:shadow-md dark:bg-celtic dark:text-secondary dark:shadow-celtic dark:transition-all dark:hover:shadow-md dark:hover:shadow-celtic xs:p-2">
-        <div className="relative h-[84px] w-[84px] xs:h-14 xs:w-14">
+        <div className="xs:h-18 xs:w-18 relative h-[84px] w-[84px]">
           <Image src={`/icons/${icon}.svg`} alt={icon} fill />
         </div>
 
-        <div className="h-[84px] space-y-2 overflow-hidden xs:h-14 xs:space-y-0">
+        <div className="h-[84px] space-y-2 xs:h-20 xs:space-y-0">
           <h3 className="overflow-ellipsis whitespace-nowrap text-xl font-bold">
             {title}
           </h3>
